bfc3bc45a69b5d8927aa4c62411392e13945b9c6
maurici0123 <108777512+maurici0123@users.noreply.github.com>
2023-08-16 13:33:11 -0300

    front-des05 final

--- exercicios/frontend_mentor/front-desafio05/script.js
+++ exercicios/frontend_mentor/front-desafio05/script.js
@@ -1,153 +1,20 @@
-keypad = document.getElementsByClassName('container')
-scren = document.getElementsByClassName('screen')
-numbe = document.getElementsByClassName('number')
-btn_reset = document.getElementById('btn_reset')
-btn_del = document.getElementById('btn_del')
-btn_equal = document.getElementById('btn_equal')
-header = document.getElementById('header')
-input = document.querySelector('input')
-styleElement = document.createElement('style');
+number = document.getElementById('number')
+resul = ''
 
-btn0 = document.getElementById('btn0')
-btn1 = document.getElementById('btn1')
-btn2 = document.getElementById('btn2')
-btn3 = document.getElementById('btn3')
-btn4 = document.getElementById('btn4')
-btn5 = document.getElementById('btn5')
-btn6 = document.getElementById('btn6')
-btn7 = document.getElementById('btn7')
-btn8 = document.getElementById('btn8')
-btn9 = document.getElementById('btn9')
-btn_minus = document.getElementById('btn_minus')
-btn_more = document.getElementById('btn_more')
-btn_bar = document.getElementById('btn_bar')
-btnx = document.getElementById('btnx')
-btn_point = document.getElementById('btn_point')
-result = document.getElementById('number')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-newStyle1 = document.createTextNode(`
-input::-webkit-slider-thumb {
-  background-color: hsl(6, 63%, 50%);
+function delet(){
+    resul = resul.slice(0, -1)
+    number.innerHTML = resul.replace(/\*/g, 'X')
 }
-input::-webkit-slider-thumb:active{
-  background-color: hsl(5, 96%, 68%);
-}`);
 
-newStyle2 = document.createTextNode(`
-input::-webkit-slider-thumb {
-  background-color: hsl(25, 98%, 40%);
+function calc(n1){
+    if (n1=='*'){
+        number.innerHTML += 'X'
+    } else{
+        number.innerHTML += n1
+    }
+    resul += n1
 }
-input::-webkit-slider-thumb:active{
-  background-color: hsl(24, 97%, 62%);
-}`);
 
-newStyle3 = document.createTextNode(`
-input::-webkit-slider-thumb {
-  background-color: hsl(176, 100%, 44%);
+ function final(){
+    number.innerHTML = eval(resul)
 }
-input::-webkit-slider-thumb:active{
-  background-color: hsl(176, 60%, 77%);
-}`);
-
-document.querySelector('input').addEventListener('input', function(){
-
-
-  // style 1 --------------------------------------
-  if (this.value == 1) {
-  document.body.style.backgroundColor = 'hsl(222, 26%, 31%)'
-  keypad[0].style.backgroundColor = 'hsl(223, 31%, 20%)'
-  scren[0].style.backgroundColor = 'hsl(224, 36%, 15%)'    
-  scren[0].style.color = 'hsl(0, 0%, 100%)'   
-
-  for (let i = 0; i < numbe.length; i++) {
-    numbe[i].style.backgroundColor = 'hsl(30, 25%, 89%)'
-    numbe[i].style.boxShadow = '0 5px 0 hsl(28, 16%, 65%)'
-    numbe[i].style.color = 'hsl(221, 14%, 31%)'
-  }
-  btn_reset.style.backgroundColor = 'hsl(225, 21%, 49%)'
-  btn_reset.style.boxShadow = '0 5px 0 hsl(224, 28%, 35%)'
-  btn_del.style.backgroundColor = 'hsl(225, 21%, 49%)'
-  btn_del.style.boxShadow = '0 5px 0 hsl(224, 28%, 35%)'
-
-  btn_equal.style.backgroundColor = ' hsl(6, 63%, 50%)'
-  btn_equal.style.boxShadow = '0 5px 0 hsl(6, 70%, 34%)'
-  btn_equal.style.color = 'hsl(0, 0%, 100%)'
-
-  header.style.color = 'hsl(0, 0%, 100%)'
-
-  input.style.backgroundColor = 'hsl(223, 31%, 20%)'
-  styleElement.appendChild(newStyle1);
-  document.head.appendChild(styleElement);
-
-
-  // style 2 -------------------------------------------
-  }else if (this.value == 2) {
-  document.body.style.backgroundColor = 'hsl(0, 0%, 90%)'
-  keypad[0].style.backgroundColor = 'hsl(0, 5%, 81%)'
-  scren[0].style.backgroundColor = 'hsl(0, 0%, 93%)'
-  scren[0].style.color = 'hsl(60, 10%, 19%)'   
-
-  for (let i = 0; i < numbe.length; i++) {
-    numbe[i].style.backgroundColor = 'hsl(45, 7%, 89%)'
-    numbe[i].style.boxShadow = '0 5px 0 hsl(35, 11%, 61%)'
-    numbe[i].style.color = 'hsl(60, 10%, 19%)'
-  }
-  btn_reset.style.backgroundColor = ' hsl(185, 42%, 37%)'
-  btn_reset.style.boxShadow = '0 5px 0 hsl(185, 58%, 25%)'
-  btn_del.style.backgroundColor = ' hsl(185, 42%, 37%)'
-  btn_del.style.boxShadow = '0 5px 0 hsl(185, 58%, 25%)'
-
-  btn_equal.style.backgroundColor = 'hsl(25, 98%, 40%)'
-  btn_equal.style.boxShadow = '0 5px 0 hsl(25, 99%, 27%)'
-  btn_equal.style.color = 'hsl(0, 0%, 100%)'
-
-  header.style.color = 'hsl(60, 10%, 19%)'
-
-  input.style.backgroundColor = 'hsl(0, 5%, 81%)'
-  styleElement.appendChild(newStyle2);
-  document.head.appendChild(styleElement);
-
-
-  // style 3 -----------------------------------------
-  }else if (this.value == 3) {
-  document.body.style.backgroundColor = 'hsl(268, 75%, 9%)'
-  keypad[0].style.backgroundColor = 'hsl(268, 71%, 12%)'
-  scren[0].style.backgroundColor = 'hsl(268, 71%, 12%)'
-  scren[0].style.color = 'hsl(52, 100%, 62%)'   
-
-  for (let i = 0; i < numbe.length; i++) {
-    numbe[i].style.backgroundColor = 'hsl(268, 47%, 21%)'
-    numbe[i].style.boxShadow = '0 5px 0 hsl(290, 70%, 36%)'
-    numbe[i].style.color = 'hsl(52, 100%, 62%)'
-  }
-  btn_reset.style.backgroundColor = ' hsl(281, 89%, 26%)'
-  btn_reset.style.boxShadow = '0 5px 0 hsl(285, 91%, 52%)'
-  btn_del.style.backgroundColor = ' hsl(281, 89%, 26%)'
-  btn_del.style.boxShadow = '0 5px 0 hsl(285, 91%, 52%)'
-
-  btn_equal.style.backgroundColor = 'hsl(176, 100%, 44%)'
-  btn_equal.style.boxShadow = '0 5px 0 hsl(177, 92%, 70%)'
-  btn_equal.style.color = 'hsl(198, 20%, 13%)'
-
-  header.style.color = 'hsl(52, 100%, 62%)'
-
-  input.style.backgroundColor = 'hsl(268, 71%, 12%)'
-  styleElement.appendChild(newStyle3);
-  document.head.appendChild(styleElement);
-  }
-})
