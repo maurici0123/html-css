f88f61c49b75b5ed0875ab9e330ddab565ad6a91
maurici0123 <108777512+maurici0123@users.noreply.github.com>
2023-08-16 16:48:54 -0300

    des05 atu

--- exercicios/frontend_mentor/front-desafio05/script.js
+++ exercicios/frontend_mentor/front-desafio05/script.js
@@ -1,20 +1,42 @@
 number = document.getElementById('number')
 resul = ''
+r = 0
 
 function delet(){
+    resul = String(resul)
     resul = resul.slice(0, -1)
+    
     number.innerHTML = resul.replace(/\*/g, 'X')
 }
 
 function calc(n1){
-    if (n1=='*'){
-        number.innerHTML += 'X'
+    if (r == 0) {
+        if (n1=='*'){
+            number.innerHTML += 'X'
+        } else{
+            number.innerHTML += n1
+        }
+        resul += n1   
+
     } else{
-        number.innerHTML += n1
+        if (n1=='*'){
+            number.innerHTML = 'X'
+        } else{
+            number.innerHTML = n1
+        }
+        resul = ''
+        resul += n1
+        r = 0
     }
-    resul += n1
 }
 
- function final(){
+ function final(){ 
+    resul = eval(resul)
     number.innerHTML = eval(resul)
+    r=1
+
+    if (typeof(resul) == 'undefined'){
+        resul = ''
+        number.innerHTML = ''
+    }
 }
