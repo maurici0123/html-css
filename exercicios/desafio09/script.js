53c7482770d7907861edd2f2154ac3b2cdaa7152
maurici0123 <108777512+maurici0123@users.noreply.github.com>
2023-09-02 23:40:57 -0300

    clima

---
+++ exercicios/desafio09/script.js
@@ -0,0 +1,58 @@
+const key = 'e19136809fdaf25296e951f9f772d11e'
+const urlcountry = 'https://flagcdn.com/h20/'
+
+const cityinput = document.getElementById('city-input')
+const searchbtn = document.getElementById('search')
+
+const cityelement = document.getElementById('city')
+const tempelement = document.querySelector('#temperature span')
+const descelement = document.getElementById('description')
+const weathericonelement = document.getElementById('weather-icon')
+const countryelement = document.querySelector('#country')
+const humidityelement = document.querySelector('#humidity span')
+const windelement = document.querySelector('#wind span')
+
+const weathercontainer = document.getElementById('weather-data')
+
+const getweatherdata = async (city) => {
+    const urlweather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}&lang=pt_br`
+
+    const res = await fetch(urlweather)
+    const data = await res.json()
+
+    return data
+}
+
+const weatherdata = async (city) => {
+    const data = await getweatherdata(city)
+
+    if (data.cod == 200) {
+
+        cityelement.innerText = data.name
+        tempelement.innerText = parseInt(data.main.temp)
+        descelement.innerText = data.weather[0].description
+        weathericonelement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+        countryelement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/24.png`)
+        humidityelement.innerText = `${data.main.humidity}%`
+        windelement.innerText = `${data.wind.speed}km/h`
+
+        weathercontainer.classList.remove('hide')
+    }else {
+        console.log('esté cidade não foi encontrada')
+    }
+}
+
+searchbtn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const city = cityinput.value
+    weatherdata(city)
+})
+
+cityinput.addEventListener('keyup', (e) => {
+    if (e.code === "Enter") {
+        const city = e.target.value
+
+        weatherdata(city)
+    }
+})
