94b90efea551848765519dec6de6573b1324998d
mauricio <mauriciosilvajunior12@gmail.com>
2024-06-23 19:17:25 -0300

    delete three

--- exercicios/test/test02/script.js
+++ exercicios/test/test02/script.js
@@ -1,74 +1,85 @@
-input = `√√81*2+log10+sin45+cos45`
+input = `cos(27*3)`
 input = input.split('')
 closed_expression = 1
 insert_expression = 1
-arraySymbols = ['√', 'log', 'sin', 'cos', 'tan']
+arraySymbols = ['√', 'log', 'sin', 'cos', 'tan', 'π', '^']
+
+function delete_three(i) {
+
+    input.splice(i, 1)
+    input.splice(i, 1)
+    input.splice(i, 1)
+}
 
 function verification(operator, i, op) {
     if (input[op] == operator && op > i) {
         closed_expression++
         input.splice(op, 0, `)`)
-        return 'stop'
+        return true
     }
 }
 
-for (i = 0; i < input.length; i++) {
-    if (input[i] == 'l' && input[i + 1] == 'o') {
-        input.splice(i, 1)
-        input.splice(i, 1)
-        input.splice(i, 1)
-        input.splice(i, 0, 'log')
-    }
-    if (input[i] == 's' && input[i + 1] == 'i') {
-        input.splice(i, 1)
-        input.splice(i, 1)
-        input.splice(i, 1)
-        input.splice(i, 0, 'sin')
-    }
-    if (input[i] == 'c' && input[i + 1] == 'o') {
-        input.splice(i, 1)
-        input.splice(i, 1)
-        input.splice(i, 1)
-        input.splice(i, 0, 'cos')
-    }
-    if (input[i] == 't' && input[i + 1] == 'a') {
-        input.splice(i, 1)
-        input.splice(i, 1)
-        input.splice(i, 1)
-        input.splice(i, 0, 'tan')
+function insert_parentheses() {
+    for (i = 0; i < input.length; i++) {
+        
     }
 }
 
-
-function conversion(symbol, expression) {
+function conversion(symbol, expression, no_parentheses = false) {
 
     for (i = 0; i < input.length; i++) {
         if (input[i] == symbol) {
             if (arraySymbols.includes(symbol)) {
                 input.splice(i, 1)
                 input.splice(i, 0, expression)
-            } else if (symbol == 'π') {
-                input.splice(i, 1)
-                input.splice(i, 0, expression)
-                break
+
+                no_parentheses ? null : insert_expression++
+
+                if (insert_parentheses()) break
             }
-            insert_expression++
 
             for (let op = 0; op < input.length; op++) {
-                if (verification('+', i, op) == 'stop') break
-                else if (verification('-', i, op) == 'stop') break
-                else if (verification('*', i, op) == 'stop') break
-                else if (verification('/', i, op) == 'stop') break
+                if (verification('+', i, op)) break
+                else if (verification('-', i, op)) break //?
+                else if (verification('*', i, op)) break //?
+                else if (verification('/', i, op)) break //?
             }
         }
     }
 }
 
+//////////////////////////////////////////////////
+
+for (i = 0; i < input.length; i++) {
+
+}
+
+for (i = 0; i < input.length; i++) {
+    if (input[i] == 'l' && input[i + 1] == 'o') {
+        delete_three(i)
+        input.splice(i, 0, 'log')
+    }
+    if (input[i] == 's' && input[i + 1] == 'i') {
+        delete_three(i)
+        input.splice(i, 0, 'sin')
+    }
+    if (input[i] == 'c' && input[i + 1] == 'o') {
+        delete_three(i)
+        input.splice(i, 0, 'cos')
+    }
+    if (input[i] == 't' && input[i + 1] == 'a') {
+        delete_three(i)
+        input.splice(i, 0, 'tan')
+    }
+}
+
+conversion('π', 'Math.PI', true)
+conversion('^', '**', true)
 conversion('√', 'Math.sqrt(')
-conversion('π', 'Math.PI')
 conversion('log', 'Math.log10(')
 conversion('sin', 'Math.sin(')
 conversion('cos', 'Math.cos(')
+conversion('tan', 'Math.tan(')
 
 while (insert_expression != closed_expression) {
     input.push(')')
