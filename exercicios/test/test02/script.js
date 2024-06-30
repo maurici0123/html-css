788e40811e5097fb82f0627560acd5366b188e9c
mauricio <mauriciosilvajunior12@gmail.com>
2024-06-29 23:10:02 -0300

    test_calc insert *

--- exercicios/test/test02/script.js
+++ exercicios/test/test02/script.js
@@ -1,27 +1,20 @@
-input = `cos(27*3)`
+input = `2*cos34log10`
 input = input.split('')
-closed_expression = 1
-insert_expression = 1
-arraySymbols = ['√', 'log', 'sin', 'cos', 'tan', 'π', '^']
+closed_expression = insert_expression = 0
+symbolsArray = ['√', 'log', 'sin', 'cos', 'tan', 'π', '^']
+expressionArray = ['Math.sqrt(', 'Math.log10(', 'Math.sin(', 'Math.cos(', 'Math.tan(', 'Math.PI', '**']
 
 function delete_three(i) {
-
     input.splice(i, 1)
     input.splice(i, 1)
     input.splice(i, 1)
 }
 
-function verification(operator, i, op) {
-    if (input[op] == operator && op > i) {
+function verification(operator, op) {
+    if (input[op] == operator || operator.includes(input[op])) {
         closed_expression++
         input.splice(op, 0, `)`)
         return true
-    }
-}
-
-function insert_parentheses() {
-    for (i = 0; i < input.length; i++) {
-        
     }
 }
 
@@ -29,30 +22,30 @@ function conversion(symbol, expression, no_parentheses = false) {
 
     for (i = 0; i < input.length; i++) {
         if (input[i] == symbol) {
-            if (arraySymbols.includes(symbol)) {
+            if (symbolsArray.includes(symbol)) {
                 input.splice(i, 1)
                 input.splice(i, 0, expression)
 
-                no_parentheses ? null : insert_expression++
+                if (no_parentheses) {
+                    null
+                } else {
+                    insert_expression++
 
-                if (insert_parentheses()) break
-            }
-
-            for (let op = 0; op < input.length; op++) {
-                if (verification('+', i, op)) break
-                else if (verification('-', i, op)) break //?
-                else if (verification('*', i, op)) break //?
-                else if (verification('/', i, op)) break //?
+                    for (let op = i + 1; op < input.length; op++) {
+                        if (verification('+', op)) break
+                        if (verification('-', op)) break
+                        if (verification('*', op)) break
+                        if (verification('/', op)) break
+                        if (verification(expressionArray, op)) break
+                        if (verification(symbolsArray, op)) break
+                    }
+                }
             }
         }
     }
 }
 
 //////////////////////////////////////////////////
-
-for (i = 0; i < input.length; i++) {
-
-}
 
 for (i = 0; i < input.length; i++) {
     if (input[i] == 'l' && input[i + 1] == 'o') {
@@ -84,6 +77,17 @@ conversion('tan', 'Math.tan(')
 while (insert_expression != closed_expression) {
     input.push(')')
     closed_expression++
+}
+
+for (let i = 0; i < input.length - 1; i++) {
+    if (input[i] == ')' && input[i + 1] == '(') input.splice(i + 1, 0, '*')
+
+    if (!isNaN(input[i]) && input[i + 1] == '(') input.splice(i + 1, 0, '*')
+    if (input[i] == ')' && !isNaN(input[i + 1])) input.splice(i + 1, 0, '*')
+
+    if (!isNaN(input[i]) && input[i + 1].match(/Math\.\w+/)) input.splice(i + 1, 0, '*')
+
+    if (input[i] == ')' && input[i + 1].match(/Math\.\w+/)) input.splice(i + 1, 0, '*')
 }
 
 input = input.join('')
