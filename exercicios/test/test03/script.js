7a57d04e4a59909dec0c5583532ffaf5950953b1
mauricio <mauriciosilvajunior12@gmail.com>
2024-08-02 19:32:26 -0300

    atu calc

--- exercicios/test/test03/script.js
+++ exercicios/test/test03/script.js
@@ -1,12 +1,12 @@
 const url = 'http://127.0.0.1:1880/api'
 
 function api() {
-    obj = fetch(url).then(res => res.json())
-    return obj
+  obj = fetch(url).then(res => res.json())
+  return obj
 }
 async function show() {
-    const tes = await api()
-    console.log(tes)
+  const tes = await api()
+  console.log(tes)
 
   const tag = document.createElement("h1");
 
